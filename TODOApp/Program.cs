4be58401dc7541ba0340e68bf596d22c51918327
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-03-13 18:38:22 -0300

    :wrench: chore: Configurando HtppClient

--- TODOApp/Program.cs
+++ TODOApp/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TODOApp;
+using TODOApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddHttpClient("API", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["APIServer"]);
+    client.DefaultRequestHeaders.Add("Accept", "application/json"); 
+});
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<UserAPI>();
 
 await builder.Build().RunAsync();
