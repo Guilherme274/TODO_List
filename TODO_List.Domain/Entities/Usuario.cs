6079223d953d5ab4f73274c8438e58b9e4c4a2a5
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-03-14 19:14:21 -0300

    :sparkles: feat: Adicionando propriedades de Nome e DataNascimento com as respectivas colunas da Tabela

--- TODO_List.Domain/Entities/Usuario.cs
+++ TODO_List.Domain/Entities/Usuario.cs
@@ -1,10 +1,14 @@
 
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace TODO_List.Domain.Entities
 {
     public class Usuario : IdentityUser
     {
-        
+        [Column("nm_usuario", TypeName = "varchar(60)")]
+        public string Nome { get; set; }
+        [Column("dt_nascimento", TypeName = "date")]
+        public string DataNascimento { get; set; }
     }
 }
