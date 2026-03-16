676d01937e75911462da4a745931cbf81a0b1cfe
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-03-15 23:38:32 -0300

    :wrench: feat: Configurando utilização de packages do projeto

--- TODO_List.API/Program.cs
+++ TODO_List.API/Program.cs
@@ -1,10 +1,86 @@
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using TODO_List.Application.Profiles;
+using TODO_List.Application.Security.Profiles;
+using TODO_List.Application.Service;
+using TODO_List.Domain.Entities;
+using TODO_List.Infraestructure.Data;
+using TODO_List.Infraestructure.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Insira o token JWT desta maneira: Bearer seu_token_aqui"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+builder.Services.AddScoped<IUsuarioService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+string connString = builder.Configuration.GetConnectionString("DbConnection");
+
+builder.Services.AddAutoMapper(
+    typeof(UsuarioProfile).Assembly,
+    typeof(TarefaProfile).Assembly
+);
+
+builder.Services.AddIdentity<Usuario, IdentityRole>()
+                            .AddEntityFrameworkStores<AppDbContext>()
+                            .AddDefaultTokenProviders();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+   options.UseNpgsql(connString); 
+});
+
+builder.Services.AddAuthentication(options =>
+{
+   options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes($"{builder.Configuration["JwtSettings:Secret"]}")),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -17,5 +93,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthorization();
+app.UseAuthentication();
+app.UseCors(options =>
+{
+   options.AllowAnyHeader()
+          .AllowAnyMethod()
+          .AllowAnyOrigin(); 
+});
+ 
 app.Run();
