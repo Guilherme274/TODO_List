8d70471247d538f2c6ca6f4a1b4e3490de574e61
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-03-15 23:33:54 -0300

    :recycle: refactor: Definindo entidade Tarefa com modificador de acesso public

--- TODO_List.Domain/Entities/Tarefa.cs
+++ TODO_List.Domain/Entities/Tarefa.cs
@@ -3,11 +3,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TODO_List.Domain.Entities
 {
-    class Tarefa
+    public class Tarefa
     {
         [Column("cd_id", TypeName = "int")]
         public int Id { get; set; }
         [Column("ds_tarefa", TypeName = "varchar(40)")]
         public string Descricao { get; set; }
-    }    
+    }
 }
